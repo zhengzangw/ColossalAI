7d5b3e63a1b338b8e389a161f6b4f704f8b68cfc
oahzxl <xuanlei.zhao@gmail.com>
2023-05-30 10:53:47 +0800

    update smoothquant

--- applications/Chat/evaluate/gen_smoothquant.sh
+++ applications/Chat/evaluate/gen_smoothquant.sh
@@ -5,17 +5,17 @@ dataset="/home/lcxyc/data1/evaluate/ColossalAI/applications/Chat/evaluate/table/
 
 
 # 6.7b
-# model_path="/home/lczxl/.cache/huggingface/hub/models--mit-han-lab--opt-6.7b-smoothquant/snapshots/21daf5a055136f5a86c8a8e03bb28ba9ecb3a953"
-# answer_path="opt-smoothquant-6.7b"
-# torchrun --standalone --nproc_per_node=$device_number generate_answers.py \
-#     --model 'a' \
-#     --strategy ddp \
-#     --model_path $model_path \
-#     --model_name $model_name \
-#     --dataset $dataset \
-#     --batch_size 8 \
-#     --answer_path $answer_path \
-#     --max_length 1024
+model_path="/home/lczxl/.cache/huggingface/hub/models--mit-han-lab--opt-6.7b-smoothquant/snapshots/21daf5a055136f5a86c8a8e03bb28ba9ecb3a953"
+answer_path="opt-smoothquant-6.7b"
+torchrun --standalone --nproc_per_node=$device_number generate_answers.py \
+    --model 'a' \
+    --strategy ddp \
+    --model_path $model_path \
+    --model_name $model_name \
+    --dataset $dataset \
+    --batch_size 8 \
+    --answer_path $answer_path \
+    --max_length 1024
 
 
 # 13b
@@ -31,7 +31,7 @@ torchrun --standalone --nproc_per_node=$device_number generate_answers.py \
     --answer_path $answer_path \
     --max_length 1024
 
-# 13b
+# 30b
 model_path="/home/lczxl/.cache/huggingface/hub/models--mit-han-lab--opt-30b-smoothquant/snapshots/0b59c759b7e37558fb5e74176774b6eb7fa78474"
 answer_path="opt-smoothquant-30b"
 torchrun --standalone --nproc_per_node=$device_number generate_answers.py \
